9b344ca234ac7abb044fe8ab02397ad8d9c1ee6c
Joaquín Torres <jtorres@tecnoabi.com>
2022-10-06 11:47:44 +0200

    Actualización de los parámetros de configuración de Amazon Cognito

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_yAZ5jboNm',
+        userPoolClientId: '1ctp7qdoaqkel6tsg8i3ctdgiv',
+        region: 'eu-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: ''
     }
 };
