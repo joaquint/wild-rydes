2677151cfecaeda3476aebc045ef071963962070
Joaquín Torres <jtorres@tecnoabi.com>
2022-10-06 12:19:15 +0200

    Actualización de los parámetros de configuración de Amazon Cognito

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 // Add your API invoke URL below, configured in Module 3. 
 
 module.exports = {
+    cognito: {
+        userPoolId: 'us-west-2_yAZ5jboNm',
+        userPoolClientId: '1ctp7qdoaqkel6tsg8i3ctdgiv',
+        region: 'eu-west-2'
+    },
     api: {
         invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
